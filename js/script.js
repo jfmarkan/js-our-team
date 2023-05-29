3c2941277c0a506d893c9620a33c6d1e07a6a1bc
jfmarkan <jfmarkan@gmail.com>
2023-05-29 13:45:03 +0200

    Milestone 1 done with a for

--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
 const teamMembers = [
     {firstName:'Wayne', lastName: 'Barnett', position:'Founder & CEO', img:'wayne-barnett-founder-ceo.jpg'},
-    {firstName:'Angela ', lastName: 'Caroll', position:'Chief Editor', img:'angela-caroll-chief-editor.jpg'},
+    {firstName:'Angela', lastName: 'Caroll', position:'Chief Editor', img:'angela-caroll-chief-editor.jpg'},
     {firstName:'Walter', lastName: 'Gordon', position:'Office Manager', img:'walter-gordon-office-manager.jpg'},
     {firstName:'Angela', lastName: 'Lopez', position:'Social Media Manager', img:'angela-lopez-social-media-manager.jpg'},
     {firstName:'Scott', lastName: 'Estrada', position:'Developer', img:'scott-estrada-developer.jpg'},
     {firstName:'Barbara', lastName: 'Ramos', position:'Graphic Designer', img:'barbara-ramos-graphic-designer.jpg'},
 ];
+
+for (let i = 0; i < teamMembers.length; i++){
+    console.log('Name: ' + teamMembers[i].firstName + " " + teamMembers[i].lastName);
+    console.log('position: ' + teamMembers[i].position);
+    console.log('img: ' + teamMembers[i].img)
+    console.log('-----')
+    }
